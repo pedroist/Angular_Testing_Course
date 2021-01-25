9a6345c7410778218c31d75b034b8d21802e1f81
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2021-01-25 19:12:02 +0000

    Lesson 12 - Service testing step by step

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,10 +1,11 @@
+import { COURSES } from './../../../../server/db-data';
 import { CoursesService } from './courses.service';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe("CoursesService", () => {
     let coursesService: CoursesService,
-        testingController: HttpTestingController;
+    httpTestingController: HttpTestingController;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -15,10 +16,34 @@ describe("CoursesService", () => {
         });
 
         coursesService = TestBed.inject(CoursesService);
-        testingController = TestBed.inject(HttpTestingController); //To specify test data
+        httpTestingController = TestBed.inject(HttpTestingController); //To specify test data
     });
 
     it("should retrieve all courses", () => {
+        coursesService.findAllCourses()
+            .subscribe(courses => {
 
+                //Check if returns null or undefined
+                expect(courses).toBeTruthy('No courses returned'); 
+
+                //Check if number or courses is correct
+                expect(courses.length).toBe(12, 'Incorrect number of courses');
+
+                const course = courses.find(course => course.id == 12);
+
+                //Check if properties received are correct
+                expect(course.titles.description).toBe('Angular Testing Course');
+            })
+        
+        //Check if the HTTP calls are made correctly
+        //only one call to this url
+        const req = httpTestingController.expectOne('/api/courses'); //req is the mock request
+
+        //it was a GET request
+        expect(req.request.method).toEqual('GET');
+
+        //pass some Test Data to the mock request
+        //Note: we can check the data by puting the URL in the browser http://localhost:9000/api/courses 
+        req.flush({payload: Object.values(COURSES)});
     });
 });
