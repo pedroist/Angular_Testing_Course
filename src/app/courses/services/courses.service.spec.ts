cabd94b79ae6df680971146b0c491bd3c26b86b0
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2021-01-25 19:27:25 +0000

    Lesson 13 - prevent other unintended HTTP requests

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -3,7 +3,7 @@ import { CoursesService } from './courses.service';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-describe("CoursesService", () => {
+describe('CoursesService', () => {
     let coursesService: CoursesService,
     httpTestingController: HttpTestingController;
 
@@ -19,7 +19,7 @@ describe("CoursesService", () => {
         httpTestingController = TestBed.inject(HttpTestingController); //To specify test data
     });
 
-    it("should retrieve all courses", () => {
+    it('should retrieve all courses', () => {
         coursesService.findAllCourses()
             .subscribe(courses => {
 
@@ -33,7 +33,7 @@ describe("CoursesService", () => {
 
                 //Check if properties received are correct
                 expect(course.titles.description).toBe('Angular Testing Course');
-            })
+            });
         
         //Check if the HTTP calls are made correctly
         //only one call to this url
@@ -45,5 +45,25 @@ describe("CoursesService", () => {
         //pass some Test Data to the mock request
         //Note: we can check the data by puting the URL in the browser http://localhost:9000/api/courses 
         req.flush({payload: Object.values(COURSES)});
+    });
+
+    it('should find a course by id', () => {
+        coursesService.findCourseById(12)
+            .subscribe(course => {
+
+                expect(course).toBeTruthy();
+                expect(course.id).toBe(12);
+            });
+        
+        const req = httpTestingController.expectOne('/api/courses/12');
+
+        expect(req.request.method).toEqual('GET');
+
+        req.flush(COURSES[12]);
+    })
+
+    afterEach(() => {
+        //Insure that no other unintended HTTP requests are made, we should call this at the end of each test
+        httpTestingController.verify();
     });
 });
